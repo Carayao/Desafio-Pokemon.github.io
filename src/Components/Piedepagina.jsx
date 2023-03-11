57d61cafd9130f6c74657e1be07b5d42ba28f704
consu <consuelo.araya.o@gmail.com>
2023-03-10 21:07:33 -0300

    first commit

---
+++ src/Components/Piedepagina.jsx
@@ -0,0 +1,10 @@
+const Piedepagina = () => {
+    return (
+        <div className='text-center text-dark p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' , position: "absolute", bottom:"0", width:"100%", height:"40px" }}>
+        © 2023 Copyright: Consuelo Araya
+        <a className='text-dark' href='https://mdbootstrap.com/'></a>
+      </div>
+    );
+  }
+  
+  export default Piedepagina;
